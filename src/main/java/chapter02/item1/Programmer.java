7825dcea30b0aad3fa3911a63f51f57b08e96d1c
Mahendra Thapa <Mahendra@fusemachines.com>
2017-02-07 22:07:04 +0545

    Implement static factor method instead of constructors.

---
+++ src/main/java/chapter02/item1/Programmer.java
@@ -0,0 +1,32 @@
+package chapter02.item1;
+
+public class Programmer {
+
+	private boolean life;
+	
+	private Programmer(boolean life){
+		this.life = life;
+	}
+	
+	public static Programmer createProgrammerWithLife(){
+		return new Programmer(true);
+	}
+
+	public static Programmer createProgrammerWithoutLife(){
+		return new Programmer(false);
+	}
+	
+	public boolean getLife(){
+		return life;
+	}
+	
+	public static void main(String[] args){
+	
+		Programmer lifeProgrammer = Programmer.createProgrammerWithLife();
+		Programmer withoutLifeProgrammer = Programmer.createProgrammerWithoutLife();
+		
+		System.out.println(lifeProgrammer.getLife());
+		System.out.println(withoutLifeProgrammer.getLife());
+		
+	}
+}
